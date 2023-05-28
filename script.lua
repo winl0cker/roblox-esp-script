5595364ecf2a23b70c297ddedcc93d07330181be
winlocker <132992225+winl0cker@users.noreply.github.com>
2023-05-29 03:07:12 +1000

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "ESP Tutorial", HidePremium = false, SaveConfig = true, ConfigFolder = "Cfg"})
+
+OrionLib:Init()
